4826bf8dd3a467b6c8765fdd1f9b69564e5330d8
zhubang <44744810+zhubang@users.noreply.github.com>
2023-02-28 14:29:22 +0800

    Update iFreetime.js

--- iFreetime.js
+++ iFreetime.js
@@ -1,5 +1,5 @@
 #!name=爱阅三秒跳广告
-#!desc="卡广告领奖励,请开启MITM或者HTTPS解密(更新日期:2023.02.26)"
+#!desc="卡广告领奖励,请开启MITM或者HTTPS解密(更新日期:2023.02.28)"
 [Rule]
 # DOMAIN,v.gdt.qq.com,REJECT
 DOMAIN,video-dsp.pddpic.com,REJECT
@@ -14,13 +14,14 @@ DOMAIN-SUFFIX,umengcloud.com,REJECT
 DOMAIN-SUFFIX,bytescm.com,REJECT
 DOMAIN-SUFFIX,ctobsnssdk.com,REJECT
 DOMAIN-SUFFIX,snssdk.com,REJECT
-URL-REGEX,^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/(stats|settings)\/,REJECT
-URL-REGEX,^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/union\/endcard\/.+\/\?.+,REJECT
-URL-REGEX,^https?:\/\/.+\.pinduoduo\.com\/marketing_api\/.+\/.+\.(png|jpeg|jpg|mp4),REJECT
-URL-REGEX,^https?:\/\/googleads\..+\.doubleclick\.net\/mads\/(gma|static\/mad\/sdk\/native).+,REJECT
-URL-REGEX,^https?:\/\/.+\.googleadservices\.com\/pagead\/.+\?.+,REJECT
-URL-REGEX,^https?:\/\/.+\.gdt.qq.com\/.+\.fcg\?.+,REJECT
-URL-REGEX,^https?:\/\/.+\.ugdtimg\.com\/gdt\/0\/.+\.(png|jpeg|jpg|mp4),REJECT
-URL-REGEX,^https?:\/\/.+\.ugdtimg\.com\/ads_svp_video__.+\.f.+\.(png|jpeg|jpg|mp4)\?.+,REJECT
+[URL Rewrite]
+^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/(stats|settings)\/.+ REJECT-DICT
+^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/union\/endcard\/.+\/\?.+ REJECT-200
+^https?:\/\/.+\.pinduoduo\.com\/marketing_api\/.+\/.+\.(png|jpeg|jpg|mp4) REJECT-200
+^https?:\/\/googleads\..+\.doubleclick\.net\/mads\/(gma|static\/mad\/sdk\/native).+ REJECT-200
+^https?:\/\/.+\.googleadservices\.com\/pagead\/.+\?.+ REJECT-200
+^https?:\/\/.+\.gdt.qq.com\/.+\.fcg\?.+ REJECT-200
+^https?:\/\/.+\.ugdtimg\.com\/gdt\/0\/.+\.(png|jpeg|jpg|mp4) REJECT-200
+^https?:\/\/.+\.ugdtimg\.com\/ads_svp_video.+\.f.+\.(png|jpeg|jpg|mp4)\?.+ REJECT-200
 [MITM]
 hostname = %APPEND% *.gdt.qq.com,*.pinduoduo.com,video-dsp.pddpic.com,*.pglstatp-toutiao.com,*.pangolin-sdk-toutiao.com,*.doubleclick.net,*.googleadservices.com,*.ugdtimg.com
