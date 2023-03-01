037ad515ecb46b2573a1e1b7d4537eafec2edcb0
zhubang <44744810+zhubang@users.noreply.github.com>
2023-03-01 15:52:29 +0800

    Update iFreetime.js

--- iFreetime.js
+++ iFreetime.js
@@ -1,7 +1,7 @@
 #!name=爱阅三秒跳广告
-#!desc="卡广告领奖励,请开启MITM或者HTTPS解密(更新日期:2023.02.28)"
+#!desc="卡广告领奖励,请开启MITM或者HTTPS解密(更新日期:2023.03.01)"
 [Rule]
-# DOMAIN,v.gdt.qq.com,REJECT
+DOMAIN,v.gdt.qq.com,REJECT
 DOMAIN,video-dsp.pddpic.com,REJECT
 DOMAIN,adim.pinduoduo.com,REJECT
 DOMAIN,images.pinduoduo.com,REJECT
@@ -15,13 +15,15 @@ DOMAIN-SUFFIX,bytescm.com,REJECT
 DOMAIN-SUFFIX,ctobsnssdk.com,REJECT
 DOMAIN-SUFFIX,snssdk.com,REJECT
 [URL Rewrite]
-^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/(stats|settings)\/.+ REJECT-DICT
-^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/union\/endcard\/.+\/\?.+ REJECT-200
-^https?:\/\/.+\.pinduoduo\.com\/marketing_api\/.+\/.+\.(png|jpeg|jpg|mp4) REJECT-200
-^https?:\/\/googleads\..+\.doubleclick\.net\/mads\/(gma|static\/mad\/sdk\/native).+ REJECT-200
-^https?:\/\/.+\.googleadservices\.com\/pagead\/.+\?.+ REJECT-200
-^https?:\/\/.+\.gdt.qq.com\/.+\.fcg\?.+ REJECT-200
-^https?:\/\/.+\.ugdtimg\.com\/gdt\/0\/.+\.(png|jpeg|jpg|mp4) REJECT-200
-^https?:\/\/.+\.ugdtimg\.com\/ads_svp_video.+\.f.+\.(png|jpeg|jpg|mp4)\?.+ REJECT-200
+^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/(stats|settings)\/.+ _ REJECT-DICT
+^https?:\/\/.+\.pangolin-sdk-toutiao\.com\/union\/endcard\/.+\/\?.+ _ REJECT-200
+^https?:\/\/.+\.pinduoduo\.com\/marketing_api\/.+\/.+\.(png|jpeg|jpg|mp4) _ REJECT-200
+^https?:\/\/googleads\..+\.doubleclick\.net\/mads\/(gma|static\/mad\/sdk\/native).+ _ REJECT-200
+^https?:\/\/.+\.googleadservices\.com\/pagead\/.+\?.+ _ REJECT-200
+^https?:\/\/.+\.gdt.qq.com\/.+\.fcg\?.+ _ REJECT-200
+^https?:\/\/.+\.ugdtimg\.com\/gdt\/0\/.+\.(png|jpeg|jpg|mp4) _ REJECT-200
+^https?:\/\/.+\.ugdtimg\.com\/ads_svp_video__.+\.f.+\.(png|jpeg|jpg|mp4)\?.+ _ REJECT-200
+[Script]
+广告联盟json = type=http-response,script-path=https://gist.githubusercontent.com/Mattraks/b243f3ac4b3184f102bd4d2ba5b8698d/raw/adsense.js,pattern=^https:\/\/(api-access\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/get_ads|open\.e\.kuaishou\.com\/rest\/e\/v3\/open\/univ$|mi\.gdt\.qq\.com\/gdt_mview\.fcg\?),requires-body=true,enable=true
 [MITM]
 hostname = %APPEND% *.gdt.qq.com,*.pinduoduo.com,video-dsp.pddpic.com,*.pglstatp-toutiao.com,*.pangolin-sdk-toutiao.com,*.doubleclick.net,*.googleadservices.com,*.ugdtimg.com
